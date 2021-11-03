6b39b7c94e5b258e4454a160665c8453845b4db9
yavor dashev <yavor@htmlelements.com>
2021-11-03 18:23:37 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { jqxGridModule } from 'jqwidgets-ng/jqxgrid';
+import { jqxButtonModule } from 'jqwidgets-ng/jqxbuttons';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, jqxGridModule, jqxButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
